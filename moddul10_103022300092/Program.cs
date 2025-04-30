bc8c959fea2cda8c0d84cbb43e484ef07549152a
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-30 16:59:56 +0700

    Menambahkan API Movie

--- moddul10_103022300092/Program.cs
+++ moddul10_103022300092/Program.cs
@@ -1,3 +1,6 @@
+
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +26,72 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class Movie
+{ 
+    public string Title { get; set; }
+    public string Directory { get; set; }
+    public List<string> Stars { get; set; }
+    public string Description { get; set; }
+
+    public Movie() 
+    {
+        this.Title = Title;
+        this.Directory = Directory;
+        this.Stars = new List<string>();
+        this.Description = Description;
+    }
+}
+[Route("api/[controller]")]
+[ApiController]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> daftarFilm = new List<Movie>
+    {
+            new Movie{Title = " The Shawshank Redemption",Directory ="Frank Darabont",
+                Stars = [" Tim Robbins","Morgan Freeman","Bob Gunton"],
+                Description = "A banker convicted of uxoricide forms a friendship over a " +
+                "quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion." },
+            new Movie{Title = " The Godfather",Directory ="Francis Ford Coppola",
+                Stars = [" Marlon Brando","AI Pacino","James Caan"],
+                Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son" },
+            new Movie{Title = " The Dark Knight",Directory ="Christopher Nolan",
+                Stars = [" Christian Bale", "Heath Ledger","Aaron Eckhart"],
+                Description = "When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, " +
+                "James Gordon and Harvey Dent must work together to put an end to the madness." }
+    };
+    [HttpGet]
+    public ActionResult<List<Movie>> GetMovie()
+    {
+        return Ok(daftarFilm);
+    }
+
+    [HttpGet("{idx}")]
+    public ActionResult<Movie> GetIdxMovie(int idx)
+    {
+        if (idx >= daftarFilm.Count || idx < 0)
+        {
+            return NotFound();
+        }
+        return Ok(daftarFilm[idx]);
+    }
+
+    [HttpPost]
+    public ActionResult Post([FromBody] Movie film)
+    {
+        daftarFilm.Add(film);
+        return Ok();
+    }
+
+    [HttpDelete("{idx}")]
+
+    public ActionResult Delete(int idx)
+    {
+        if (idx >= daftarFilm.Count || idx < 0)
+        {
+            return NotFound();
+        }
+        daftarFilm.RemoveAt(idx);
+        return Ok();
+    }
+}
--- moddul10_103022300092/obj/Debug/net8.0/moddul10_103022300092.AssemblyInfo.cs
+++ moddul10_103022300092/obj/Debug/net8.0/moddul10_103022300092.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moddul10_103022300092")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c41fc82f85648ab55ab4e0c9f6158cb812377da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("moddul10_103022300092")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moddul10_103022300092")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
